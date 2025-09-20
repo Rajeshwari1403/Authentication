2adbc97a495575d50532c5322534cee74c7d4025
Rajeshwari1403 <rajeshwariravi2005@gmail.com>
2025-09-20 22:25:52 +0530

    Auth

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
   connectDB();
   console.log("Server is running on port: ", PORT);
 });
-
