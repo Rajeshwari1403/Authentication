0d35f2ad4fba4ae0551d2bc7fe3d6cd240ef3959
Rajeshwari1403 <rajeshwariravi2005@gmail.com>
2025-09-20 17:05:06 +0530

    Auth

---
+++ backend/controllers/auth_contoller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("SignUp Route");
+}
+
+export const login = async (req, res) => {
+  res.send("Login Route");
+}
+
+export const logout = async (req, res) => {
+  res.send("Logout Route");
+}
